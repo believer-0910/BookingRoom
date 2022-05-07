4d4b7a1f764706a663948dad568acbdb189bb8db
Saidamin Qambarov <93591690+believer-0910@users.noreply.github.com>
2022-05-07 13:23:40 +0300

    Add files via upload

---
+++ BookingRoom/src/main/java/com/exadel/BookingRoom/BookingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.exadel.BookingRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingRoomApplication.class, args);
+	}
+
+}
---
+++ BookingRoom/src/test/java/com/exadel/BookingRoom/BookingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.BookingRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
